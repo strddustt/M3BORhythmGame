8a4acbda63b32f8546be07b0146dc24071635f43
Penny <38731@ma-web.nl>
2025-02-28 10:19:04 +0100

    Rotation af

--- M3BO rhythm game/Assets/Scripts/BarrierRotation.cs
+++ M3BO rhythm game/Assets/Scripts/BarrierRotation.cs
@@ -5,13 +5,6 @@ using UnityEngine;
 public class BarrierRotation : MonoBehaviour
 {
     public int RotationSpeed = 10;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
         Rotation();
